ef5cc8811cce26f48132c59d5c51e9f8c20f5435
revanth <revanthkrishna.maddula@sjsu.edu>
2020-11-17 01:22:38 -0800

    login page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Form } from "react-bootstrap";
-import axios from 'axios';
+import axios from "axios";
 import { Redirect } from "react-router";
-import {backendServer} from '../../webconfig.js'
+import { backendServer } from "../../webconfig.js";
+import "../Login/login.css";
 
 import { Link } from "react-router-dom";
 
@@ -20,183 +21,164 @@ class Login extends Component {
     this.emailChangeHandler = this.emailChangeHandler.bind(this);
     this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
-    this.handleChange = this.handleChange.bind(this);  
-}
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-componentWillMount() {
+  componentWillMount() {
     this.setState({
-        authFlag: false
-    })
-}
+      authFlag: false,
+    });
+  }
 
-emailChangeHandler = (e) => {
+  emailChangeHandler = (e) => {
     this.setState({
-        email: e.target.value
-    })
-}
+      email: e.target.value,
+    });
+  };
 
-passwordChangeHandler = (e) => {
+  passwordChangeHandler = (e) => {
     this.setState({
-        password: e.target.value
-    })
-}
+      password: e.target.value,
+    });
+  };
 
-
-userTypeChangeHandler = (e) => {
+  userTypeChangeHandler = (e) => {
     this.setState({
-        type: e.target.value
-    })
-}
+      type: e.target.value,
+    });
+  };
 
-
-handleChange = (e) => {
-    console.log('e', e.target.name);
-    console.log('e', e.target.value);
-    if(this.state.showLoginError){
-        this.setState({
-            showLoginError: false
-        })
+  handleChange = (e) => {
+    console.log("e", e.target.name);
+    console.log("e", e.target.value);
+    if (this.state.showLoginError) {
+      this.setState({
+        showLoginError: false,
+      });
     }
-    this.setState({ 
-        [e.target.name]: e.target.value
-    })
-}
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-
-handleLogin = (e) => {
+  handleLogin = (e) => {
     const form = document.getElementById("signIn");
     form.reportValidity();
     if (form.checkValidity()) {
-        //prevent page from refresh
-        e.preventDefault();
-        const data = {
-            email: this.state.email,
-            password: this.state.password
-        }
-        //set the with credentials to true
-        axios.defaults.withCredentials = true;
-        //make a post request with the user data
-        console.log('req.body', data);
-        axios.post(`${backendServer}/login/`, data)
-            .then(response => {
-                if(response){
-                    localStorage.setItem("token", response.data.token);
-                    localStorage.setItem("id", response.data._id);
-                    localStorage.setItem("name", response.data.name);
-                    localStorage.setItem("type", response.data.userType);
-                    if(response.data.userType === 'buyer'){
-                        window.location.href = "/userdashboard";
-                    }
-                    else if(response.data.userType === 'Seller'){
-                        window.location.href = "/sellerinventory";
-                    }
-                    else{
-                        window.location.href = "/admin-dashboard";
-                    }
-                    this.setState({ redirectToHome: true });
-                    this.setState({
-                        authFlag: true
-                    })
-                }else{
-                    this.setState({
-                        showLoginError: true
-                    })
-                }
+      //prevent page from refresh
+      e.preventDefault();
+      const data = {
+        email: this.state.email,
+        password: this.state.password,
+      };
+      //set the with credentials to true
+      axios.defaults.withCredentials = true;
+      //make a post request with the user data
+      console.log("req.body", data);
+      axios
+        .post(`${backendServer}/login/`, data)
+        .then((response) => {
+          if (response) {
+            localStorage.setItem("token", response.data.token);
+            localStorage.setItem("id", response.data._id);
+            localStorage.setItem("name", response.data.name);
+            localStorage.setItem("type", response.data.userType);
+            if (response.data.userType === "buyer") {
+              window.location.href = "/userdashboard";
+            } else if (response.data.userType === "Seller") {
+              window.location.href = "/sellerinventory";
+            } else {
+              window.location.href = "/admin-dashboard";
             }
-            ).catch(ex => {
-                this.setState({
-                    showLoginError: true
-                })
+            this.setState({ redirectToHome: true });
+            this.setState({
+              authFlag: true,
             });
+          } else {
+            this.setState({
+              showLoginError: true,
+            });
+          }
+        })
+        .catch((ex) => {
+          this.setState({
+            showLoginError: true,
+          });
+        });
     }
-
-}
-
-
+  };
 
   render() {
     let redirectVar = null;
-   
 
     if (this.state.redirectToHome) {
-        redirectVar = <Redirect push to="/somewhere/else" />
+      redirectVar = <Redirect push to="/somewhere/else" />;
     }
     return (
+      <div>
+        {redirectVar}
 
         <div>
-            {redirectVar}
-       
-           
-            
-                        <div>
-                            <a href="/login">
-                               
-                                {redirectVar}
-                            </a>
-                        </div>
-                    
-                        <div>
-                         
-                            {this.state.showLoginError && 
-                            
-                                  
-                                        <div><h4>Important Message!</h4><i></i><div>
-                                            <ul>
-                                                <li><span>
-                                                    Invalid credentials. Please check your username and password.
-                                        </span></li>
-                                            </ul>
-                                        </div></div>
-                            }
-                            <div>
-                                        <form id="signIn">
+          <a href="/login">{redirectVar}</a>
+        </div>
 
-                                                    <div >
-                                                        <h1>
-                                                            Sign-In
-                          </h1>
-                                                        <div>
-                                                            <label>
-                                                                Email 
-                            </label>
-                                                            <input onChange={this.handleChange} required type="email" maxLength={128} id="ap_email" name="email"/>
-                                                          
-                                                        </div>
-                                                        <input type="hidden" name="create" />
-                                                     
-                                                                <div>
-                                                                    <label htmlFor="ap_password">
-                                                                        Password
-                                </label>
-                                <input onChange={this.handleChange} required type="password" maxLength={1024} id="ap_password" name="password" />
-
-                                                                </div>
-                                                               
-                                                           
-                                                        <div>
-                                                            <input type="button"  value = "Sign-In" onClick={this.handleLogin}  />
-                                                        </div>
-                                                        <div className="a-row">
-                                                            <span><a id="signInCancelSubmit"  href="/signup"  role="button">
-                                                                Register now
-                                </a></span>
-                                                        </div>
-                                                    </div>
-                                             
-                                        </form>
-                                    </div>
-
-                        </div>
-                  
-                  
-                </div>
-              
-             
-         
-
-    )
-}
-  
+        <div>
+          {this.state.showLoginError && (
+            <div>
+              <h4>Important Message!</h4>
+              <i></i>
+              <div>
+                <ul>
+                  <li>
+                    <span>
+                      Invalid credentials. Please check your username and
+                      password.
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+          <div>
+            <form id="signIn">
+            <div className="login-wrap">
+        <div className="login-html">
+          <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked /><label htmlFor="tab-1" className="tab">Sign In</label>
+          <input id="tab-2" type="radio" name="tab" className="for-pwd" /><label htmlFor="tab-2" className="tab">Forgot Password</label>
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label htmlFor="user" className="label">Username or Email</label>
+                <input id="user" type="text" className="input" onChange={this.handleChange} required name="email"/>
+              </div>
+              <div className="group">
+                <label htmlFor="pass" className="label">Password</label>
+                <input id="pass" type="password" className="input" data-type="password" onChange={this.handleChange} required name="password"/>
+              </div>
+              <div className="group">
+                <input type="submit" className="button" defaultValue="Sign In" onClick={this.handleLogin} />
+              </div>
+              <div className="hr" />
+            </div>
+            <div className="for-pwd-htm">
+              <div className="group">
+                <label htmlFor="user" className="label">Username or Email</label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <input type="submit" className="button" defaultValue="Reset Password" />
+              </div>
+              <div className="hr" />
+            </div>
+          </div>
+        </div>
+      </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -12,26 +12,15 @@ class Main extends Component {
     
         return (
             <div>
-                {localStorage.getItem('id') &&
+            
+               
                     <Fragment>
                     
-                        {/* {navRoute}
-                      
-                        <Route exact path="/" component={Dashboard} />
-                        <Route exact path="/product-search" component={ProductSearch} />
-
-
-                        {footer} */}
-                    </Fragment>
-                }
-                {!localStorage.getItem('id') &&
-                    <Fragment>
-                        {/* <Route exact path="/login" component={Login} /> */}
-                        <Route exact path="/" component={Login} />
+                        <Route exact path="/login" component={Login} />
                         {/* <Route exact path="/signup" component={SignUp} /> */}
                     </Fragment>
-                }
-                {/*<Route path="/login" component={Login}/>*/}
+        
+               
 
             </div>
         )
