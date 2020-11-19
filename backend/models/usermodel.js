de862b808e36e126aed1a533a01692f3f5da148e
revanth <revanthkrishna.maddula@sjsu.edu>
2020-11-18 17:17:20 -0800

    register

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -7,6 +7,7 @@ var userSchema = new Schema({
     email : {type: String, required: true},
     name : {type: String, required: true}, //To be unique for seller
     password : {type: String, required: true},
+    role: {type: String, required: true},
     usertype: {type: String , enum : ['Renter','Landlord','Buyer','Seller']},
     address: {type: String},
     token:{type:String}
